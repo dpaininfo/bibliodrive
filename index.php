b516eece708e2584a273114bec279a11ccabcb39
dpaininfo <delphinepmailbox-abo@yahoo.fr>
2020-12-02 12:14:02 +0100

    ok

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Ma bibliothèque</title>
+    </head>
+    <body>
+        <form action="index.php" method="post">
+            Login : <input type="text" name="login"/>
+            Mot de passe : <input type="password" name="mdp"/>
+            <input type="submit" name="ok" value="ok"/>
+        </form>
+    </body>
+</html>
